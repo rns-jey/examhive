d81fa4bd8c5471f697cc65b8a358147d0f948f78
JM Arenas <84744855+rns-jey@users.noreply.github.com>
2025-02-03 22:25:41 +0000

    Add shadcn toaster to the project

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -1,10 +1,12 @@
 import AppHeader from "@/components/organisms/app-header";
+import { Toaster } from "@/components/ui/toaster";
 import React from "react";
 
 export default function MainLayout({ children }: { children: React.ReactNode }) {
   return (
     <div>
       <AppHeader />
+      <Toaster />
       {children}
     </div>
   );
