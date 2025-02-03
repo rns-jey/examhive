71f13ae8eb66d430b27c4663b0c02dcbf3b3d6b7
JM Arenas <84744855+rns-jey@users.noreply.github.com>
2025-02-03 09:32:02 +0000

    Update landing page header

--- components/organisms/public-header.tsx
+++ components/organisms/public-header.tsx
@@ -1,36 +1,23 @@
-import { SignedIn, SignedOut, SignInButton, SignOutButton } from "@clerk/nextjs";
-import { GraduationCap } from "lucide-react";
+import { SignedOut, SignInButton } from "@clerk/nextjs";
+import { Hexagon } from "lucide-react";
 import Link from "next/link";
 import React from "react";
+import { Button } from "../atoms/button";
 
 export default function PublicHeader() {
   return (
-    // <div>
-    //   <SignedIn>
-    //     <SignOutButton />
-    //   </SignedIn>
-
-    //   <SignedOut>
-    //     <SignInButton />
-    //   </SignedOut>
-    // </div>
     <header className="border-b">
       <div className="container mx-auto px-4 h-16 flex items-center justify-between">
         <Link href="/" className="flex items-center space-x-2">
-          <GraduationCap className="h-6 w-6" />
-          <span className="font-bold text-xl">ExamHub</span>
+          <Hexagon className="h-10 w-10 text-yellow-500" />
+          <span className="font-bold text-xl">Exam Hive</span>
         </Link>
         <nav className="flex items-center space-x-4">
-          <Link href="/" className="text-sm font-medium hover:underline underline-offset-4">
-            Home
-          </Link>
-          <Link href="/exams" className="text-sm font-medium hover:underline underline-offset-4">
-            All Exams
-          </Link>
-          <Link href="/dashboard" className="text-sm font-medium hover:underline underline-offset-4">
-            Dashboard
-          </Link>
-          {/* <Button variant="outline">Sign In</Button> */}
+          <SignedOut>
+            <Button asChild className="bg-amber-500 hover:bg-amber-600">
+              <SignInButton>Start Practice</SignInButton>
+            </Button>
+          </SignedOut>
         </nav>
       </div>
     </header>
