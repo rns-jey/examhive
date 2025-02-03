9c8f51dd36c6b7bfa6894934ee938f751e8a630e
JM Arenas <84744855+rns-jey@users.noreply.github.com>
2025-02-03 18:35:07 +0000

    Build home page header

--- components/organisms/wrapper.tsx
+++ components/organisms/wrapper.tsx
@@ -1,5 +1,5 @@
 import React from "react";
 
 export default function Wrapper({ children }: { children: React.ReactNode }) {
-  return <div className="container mx-auto p-4">{children}</div>;
+  return <div className="container mx-auto p-4 md:p-6 lg:p-8">{children}</div>;
 }
--- components/pages/home-page.tsx
+++ components/pages/home-page.tsx
@@ -1,32 +1,15 @@
-const user = {
-  name: "Tom Cook",
-  email: "tom@example.com",
-  imageUrl:
-    "https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
-};
-const navigation = [
-  { name: "Dashboard", href: "#", current: true },
-  { name: "Team", href: "#", current: false },
-  { name: "Projects", href: "#", current: false },
-  { name: "Calendar", href: "#", current: false },
-];
-const userNavigation = [
-  { name: "Your Profile", href: "#" },
-  { name: "Settings", href: "#" },
-  { name: "Sign out", href: "#" },
-];
+import { currentUser } from "@clerk/nextjs/server";
+import Wrapper from "../organisms/wrapper";
 
-export default function HomePage() {
+export default async function HomePage() {
+  const { firstName } = await currentUser();
+
   return (
-    <div className="py-10">
-      <header>
-        <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
-          <h1 className="text-3xl font-bold tracking-tight text-gray-900">Dashboard</h1>
-        </div>
+    <Wrapper>
+      <header className="space-y-2">
+        <h1 className="font-bold text-xl tracking-tight md:text-3xl lg:text-4xl">{`Welcome back, ${firstName}!`}</h1>
+        <p className="text-muted-foreground">Pick up where you left off or try something new.</p>
       </header>
-      <main>
-        <div className="mx-auto max-w-7xl px-4 py-8 sm:px-6 lg:px-8">{/* Your content */}</div>
-      </main>
-    </div>
+    </Wrapper>
   );
 }
