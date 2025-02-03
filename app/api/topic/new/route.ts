d73e36e0beca0c8c582d56f7d3f67c66e22036b6
JM Arenas <84744855+rns-jey@users.noreply.github.com>
2025-02-03 22:35:22 +0000

    Implement create new topic feature

---
+++ app/api/topic/new/route.ts
@@ -0,0 +1,28 @@
+import currentProfile from "@/lib/current-profile";
+import { db } from "@/lib/db";
+import { NextResponse } from "next/server";
+
+export async function POST(req: Request) {
+  try {
+    const profile = await currentProfile();
+
+    if (!profile) return new NextResponse("Unauthorized", { status: 401 });
+
+    console.log(profile);
+    if (profile.role !== "ADMIN") return new NextResponse("Unauthorized", { status: 401 });
+
+    const { title, description } = await req.json();
+
+    const topic = await db.topic.create({
+      data: {
+        title,
+        description,
+      },
+    });
+
+    return NextResponse.json(topic);
+  } catch (error) {
+    console.log(error);
+    return new NextResponse("Internal Server Error", { status: 500 });
+  }
+}
--- components/organisms/new-topic-form.tsx
+++ components/organisms/new-topic-form.tsx
@@ -4,15 +4,19 @@ import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 
+import axios from "axios";
+
 import { DialogContent, DialogHeader, DialogTitle } from "../ui/dialog";
 import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "../ui/form";
 import { Input } from "../ui/input";
 import { Textarea } from "../ui/textarea";
 import { Button } from "../atoms/button";
+import { useToast } from "@/hooks/use-toast";
+import { useRouter } from "next/navigation";
 
 const formSchema = z.object({
   title: z.string().min(2).max(50),
-  description: z.string().min(2).max(50),
+  description: z.string().min(2).max(255),
 });
 
 export default function NewTopicForm() {
@@ -24,10 +28,22 @@ export default function NewTopicForm() {
     },
   });
 
-  function onSubmit(values: z.infer<typeof formSchema>) {
-    // Do something with the form values.
-    // ✅ This will be type-safe and validated.
-    console.log(values);
+  const { toast } = useToast();
+  const router = useRouter();
+
+  async function onSubmit(values: z.infer<typeof formSchema>) {
+    try {
+      const topic = await axios.post("/api/topic/new", values);
+
+      toast({
+        description: `Successfully added new topic!`,
+      });
+
+      form.reset();
+      router.push(`/exams/edit/${topic.data.id}`);
+    } catch (error) {
+      console.error(error);
+    }
   }
 
   return (
