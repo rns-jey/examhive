2abf719225b86eb1b4a0221d3890e46e6651dd3e
JM Arenas <84744855+rns-jey@users.noreply.github.com>
2025-02-04 00:41:53 +0000

    Remove await to params

--- app/(main)/exams/[examId]/edit/page.tsx
+++ app/(main)/exams/[examId]/edit/page.tsx
@@ -3,18 +3,18 @@ import currentProfile from "@/lib/current-profile";
 import { auth } from "@clerk/nextjs/server";
 import React from "react";
 
-export default async function EditTopic({
-  params,
-}: {
+interface EditTopicProps {
   params: {
     examId: string;
   };
-}) {
+}
+
+export default async function EditTopic({ params }: EditTopicProps) {
   const profile = await currentProfile();
 
   if (!profile) return (await auth()).redirectToSignIn;
 
-  const { examId } = await params;
+  const { examId } = params;
 
   return <EditTopicPage examId={examId} />;
 }
