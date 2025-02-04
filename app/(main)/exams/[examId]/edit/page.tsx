bc6afdcbf32b28ab6ee6ce9cf6f3895aae1b325c
JM Arenas <84744855+rns-jey@users.noreply.github.com>
2025-02-04 01:00:02 +0000

    Adjust params

--- app/(main)/exams/[examId]/edit/page.tsx
+++ app/(main)/exams/[examId]/edit/page.tsx
@@ -3,11 +3,7 @@ import currentProfile from "@/lib/current-profile";
 import { auth } from "@clerk/nextjs/server";
 import React from "react";
 
-interface EditTopicProps {
-  params: { examId: string };
-}
-
-export default async function EditTopic({ params }: EditTopicProps) {
+export default async function EditTopic({ params }: { params: { examId: string } }) {
   const profile = await currentProfile();
 
   if (!profile) return (await auth()).redirectToSignIn;
