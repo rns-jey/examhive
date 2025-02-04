3d471dbd6deed95cf0faa6a7ed215a3ad6c25e49
JM Arenas <84744855+rns-jey@users.noreply.github.com>
2025-02-04 01:23:06 +0000

    Fix promise params in background api

--- app/api/topic/[examId]/route.ts
+++ app/api/topic/[examId]/route.ts
@@ -2,13 +2,15 @@ import currentProfile from "@/lib/current-profile";
 import { db } from "@/lib/db";
 import { NextResponse } from "next/server";
 
-export async function GET(req: Request, { params }: { params: { examId: string } }) {
+type tParams = Promise<{ examId: string }>;
+
+export async function GET(req: Request, props: { params: tParams }) {
   try {
     const profile = await currentProfile();
 
     if (!profile) return new NextResponse("Unauthorized", { status: 401 });
 
-    const { examId } = await params;
+    const { examId } = await props.params;
 
     const topic = await db.topic.findUnique({
       where: {
