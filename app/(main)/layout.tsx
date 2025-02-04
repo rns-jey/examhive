7514fa9d1058e49f4b4e1e59581d7872673e96cd
JM Arenas <84744855+rns-jey@users.noreply.github.com>
2025-02-04 00:00:56 +0000

    Build react query provider

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -1,13 +1,16 @@
 import AppHeader from "@/components/organisms/app-header";
+import ReactQueryProvider from "@/components/providers/react-query-provider";
 import { Toaster } from "@/components/ui/toaster";
 import React from "react";
 
 export default function MainLayout({ children }: { children: React.ReactNode }) {
   return (
-    <div>
-      <AppHeader />
-      <Toaster />
-      {children}
-    </div>
+    <ReactQueryProvider>
+      <div>
+        <AppHeader />
+        <Toaster />
+        {children}
+      </div>
+    </ReactQueryProvider>
   );
 }
