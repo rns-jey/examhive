8b6e4e4efa9e190ecc904656869a3701ad950649
JM Arenas <84744855+rns-jey@users.noreply.github.com>
2025-02-05 09:38:36 +0000

    Build modal store

---
+++ hooks/use-modal-store.ts
@@ -0,0 +1,30 @@
+import { Exam } from "@prisma/client";
+import { create } from "zustand";
+
+interface ModalData {
+  exam?: Exam;
+}
+
+export type ModalType = "createExam" | "createQuestion";
+
+interface ModalStore {
+  type: ModalType | null;
+  data: ModalData;
+  isOpen: boolean;
+  onOpen: (type: ModalType, data?: ModalData) => void;
+  onClose: () => void;
+}
+
+export const useModal = create<ModalStore>((set) => ({
+  type: null,
+  data: {},
+  isOpen: false,
+
+  onOpen: (type, data = {}) => {
+    set({ type, data, isOpen: true });
+  },
+
+  onClose: () => {
+    set({ type: null, data: {}, isOpen: false });
+  },
+}));
