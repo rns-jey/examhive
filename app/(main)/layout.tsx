2193f3b9ece69a327eb168f714dd0d4678e148c1
JM Arenas <84744855+rns-jey@users.noreply.github.com>
2025-02-05 10:05:21 +0000

    Build modal provider

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -1,4 +1,5 @@
 import AppHeader from "@/components/organisms/app-header";
+import ModalProvider from "@/components/providers/modal-provider";
 import ReactQueryProvider from "@/components/providers/react-query-provider";
 import { Toaster } from "@/components/ui/toaster";
 import React from "react";
@@ -9,6 +10,7 @@ export default function MainLayout({ children }: { children: React.ReactNode }) 
       <div>
         <AppHeader />
         <Toaster />
+        <ModalProvider />
         {children}
       </div>
     </ReactQueryProvider>
---
+++ components/providers/modal-provider.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import CreateExamModal from "../organisms/modals/create-exam-modal";
+
+export default function ModalProvider() {
+  return (
+    <>
+      <CreateExamModal />
+    </>
+  );
+}
