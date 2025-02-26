fa11640dc6ab981d93c6b8656cc5ffebf8f6031c
JM Arenas <84744855+rns-jey@users.noreply.github.com>
2025-02-26 09:18:46 +0000

    Fix prefer-const error

--- app/(main)/exams/[examId]/page.tsx
+++ app/(main)/exams/[examId]/page.tsx
@@ -51,16 +51,16 @@ export default async function Exam(props: { params: tParams }) {
   }
 
   const shuffleQuestions = (questions: Question[]) => {
-    let shuffled = [...questions]; // Copy the original array to avoid mutation
+    const shuffled = [...questions]; // Copy the original array to avoid mutation
     for (let i = shuffled.length - 1; i > 0; i--) {
       const j = Math.floor(Math.random() * (i + 1)); // Pick a random index
       [shuffled[i], shuffled[j]] = [shuffled[j], shuffled[i]]; // Swap elements
     }
-    
+
     return shuffled;
   };
 
-  console.log(shuffleQuestions(exam.questions))
+  console.log(shuffleQuestions(exam.questions));
 
   return <ExamPage questions={shuffleQuestions(exam.questions)} />;
 }
